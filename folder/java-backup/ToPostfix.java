4a396263a5e19201ae2471e63132a15ab18c9120
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2022-11-01 02:23:21 +0800

    Update format

--- folder/java-backup/ToPostfix.java
+++ folder/java-backup/ToPostfix.java
@@ -8,149 +8,149 @@ import java.util.Stack;
 
 
 public class ToPostfix {
-	static Scanner keyboard = new Scanner(System.in);
+    static Scanner keyboard = new Scanner(System.in);
 
-	Map<Character, Integer> weight = new HashMap<Character, Integer>() {
-		{
-			put('-', 1);
-	        put('+', 1);
-	        put('*', 2);
-	        put('/', 2);
-	        put('%', 2);
-	        put('^', 3);
-		}
-	};
+    Map<Character, Integer> weight = new HashMap<Character, Integer>() {
+        {
+            put('-', 1);
+            put('+', 1);
+            put('*', 2);
+            put('/', 2);
+            put('%', 2);
+            put('^', 3);
+        }
+    };
 
 /************************************************************************/
-	public int InfixToPostfix(String infix) {
+    public int InfixToPostfix(String infix) {
 
-		infix = infix.replaceAll(" ", "");
-		char[] infix_ary = infix.toCharArray();
-		int infix_length = infix.length();
-		
-		Deque<Integer> nums = new ArrayDeque<>();
-		Deque<Character> opers = new ArrayDeque<>();
-		
-		nums.addLast(0);
-		
-		for(int i = 0; i < infix_length; i++) {
+        infix = infix.replaceAll(" ", "");
+        char[] infix_ary = infix.toCharArray();
+        int infix_length = infix.length();
+        
+        Deque<Integer> nums = new ArrayDeque<>();
+        Deque<Character> opers = new ArrayDeque<>();
+        
+        nums.addLast(0);
+        
+        for(int i = 0; i < infix_length; i++) {
 
-			if(infix_ary[i] == '(') 
-				opers.addLast(infix_ary[i]);
+            if(infix_ary[i] == '(') 
+                opers.addLast(infix_ary[i]);
 
-			else if(infix_ary[i] == ')') {
-				while(!opers.isEmpty()) {
-					if(opers.peekLast() != '(')
-						calculate(nums, opers);
-					else {
-						opers.pollLast();
-						break;
-					}
-				}
-			}
+            else if(infix_ary[i] == ')') {
+                while(!opers.isEmpty()) {
+                    if(opers.peekLast() != '(')
+                        calculate(nums, opers);
+                    else {
+                        opers.pollLast();
+                        break;
+                    }
+                }
+            }
 
-			else {
-				if(isNumber(infix_ary[i])) {
-					int count = 0, j = i;
-					
-					while(j < infix_length && isNumber(infix_ary[j])) {
-						count = count * 10 + (int)(infix_ary[j++] - '0');
-					}
-					i = j -1;
-					nums.addLast(count);
-				}
-				else {
-					while(!opers.isEmpty() && opers.peekLast() != '(') {
-						char opersLast = opers.peekLast();
+            else {
+                if(isNumber(infix_ary[i])) {
+                    int count = 0, j = i;
+                    
+                    while(j < infix_length && isNumber(infix_ary[j])) {
+                        count = count * 10 + (int)(infix_ary[j++] - '0');
+                    }
+                    i = j -1;
+                    nums.addLast(count);
+                }
+                else {
+                    while(!opers.isEmpty() && opers.peekLast() != '(') {
+                        char opersLast = opers.peekLast();
 
-						if(weight.get(opersLast) >= weight.get(infix_ary[i]))
-							calculate(nums, opers);
-						else
-							break;
-					}
-					opers.addLast(infix_ary[i]);
-				}
-			}
-		}
+                        if(weight.get(opersLast) >= weight.get(infix_ary[i]))
+                            calculate(nums, opers);
+                        else
+                            break;
+                    }
+                    opers.addLast(infix_ary[i]);
+                }
+            }
+        }
 
-		while(!opers.isEmpty() && opers.peekLast() != '(') {
-			calculate(nums, opers);
-		}
-		return nums.peekLast();
-	}
+        while(!opers.isEmpty() && opers.peekLast() != '(') {
+            calculate(nums, opers);
+        }
+        return nums.peekLast();
+    }
 /************************************************************************/
-	public void calculate(Deque<Integer> nums, Deque<Character> opers) {
-		if(nums.isEmpty() || nums.size() < 2) return;
-		if(opers.isEmpty()) return;
-		
-		int b = nums.pollLast(),
-			a = nums.pollLast();
-		char o = opers.pollLast();
-		int ans = 0;
-		
-		if(o == '+') ans = a + b;
-		else if(o == '-') ans = a - b;
-		else if(o == '*') ans = a * b;
-		else if(o == '/') ans = a / b;
-		else if(o == '%') ans = a % b;
-		else if(o == '^') ans = (int)Math.pow(a, b);
-		
-		nums.addLast(ans);
-	}
+    public void calculate(Deque<Integer> nums, Deque<Character> opers) {
+        if(nums.isEmpty() || nums.size() < 2) return;
+        if(opers.isEmpty()) return;
+        
+        int b = nums.pollLast(),
+            a = nums.pollLast();
+        char o = opers.pollLast();
+        int ans = 0;
+        
+        if(o == '+') ans = a + b;
+        else if(o == '-') ans = a - b;
+        else if(o == '*') ans = a * b;
+        else if(o == '/') ans = a / b;
+        else if(o == '%') ans = a % b;
+        else if(o == '^') ans = (int)Math.pow(a, b);
+        
+        nums.addLast(ans);
+    }
 /************************************************************************/
-	public boolean isNumber(char c) {
-		return Character.isDigit(c);
-	}
+    public boolean isNumber(char c) {
+        return Character.isDigit(c);
+    }
 /************************************************************************/
-	public static boolean validParenthes(ArrayList<Character> parenthes) {
+    public static boolean validParenthes(ArrayList<Character> parenthes) {
 
-		if(parenthes.size() %2 != 0) return false;
-		
-		Stack<Character> stack = new Stack<Character>();
+        if(parenthes.size() %2 != 0) return false;
+        
+        Stack<Character> stack = new Stack<Character>();
 
-		for(char c : parenthes) {
-			if(c == '(') 
-				stack.push(')');
-			else
-				if(stack.empty() || c != stack.pop()) return false;
-		}
+        for(char c : parenthes) {
+            if(c == '(') 
+                stack.push(')');
+            else
+                if(stack.empty() || c != stack.pop()) return false;
+        }
 
-		if(stack.empty()) return true;
+        if(stack.empty()) return true;
         return false;
-	}
+    }
 /************************************************************************/
 
 
-	public static void main(String[] args) {
-		ToPostfix topost = new ToPostfix();
-		
-		//String infix = "10*(9+1*6)";
-		
-		while(true) {
-			System.out.print("\n--------------------------\n");
-			System.out.print("   -- valid operands --\n");
-			System.out.print("  +, -, *, /, %, ^, (, )");
-			System.out.print("\n--------------------------\n");
+    public static void main(String[] args) {
+        ToPostfix topost = new ToPostfix();
+        
+        //String infix = "10*(9+1*6)";
+        
+        while(true) {
+            System.out.print("\n--------------------------\n");
+            System.out.print("   -- valid operands --\n");
+            System.out.print("  +, -, *, /, %, ^, (, )");
+            System.out.print("\n--------------------------\n");
 
 
-			System.out.printf("\nEnter infix: ");
-			String input = keyboard.nextLine();
+            System.out.printf("\nEnter infix: ");
+            String input = keyboard.nextLine();
 
-			char[] input_ary = input.toCharArray();
-			ArrayList<Character> parenthes = new ArrayList<Character>();
-			for(int i = 0; i < input_ary.length; i++) {
-				if(input_ary[i] == '(' || input_ary[i] == ')')
-					parenthes.add(input_ary[i]);
-			}
+            char[] input_ary = input.toCharArray();
+            ArrayList<Character> parenthes = new ArrayList<Character>();
+            for(int i = 0; i < input_ary.length; i++) {
+                if(input_ary[i] == '(' || input_ary[i] == ')')
+                    parenthes.add(input_ary[i]);
+            }
 
-			if(!validParenthes(parenthes)) {
-				System.out.println("\nInvalid parentheses, try again");
-				continue;
-			}
+            if(!validParenthes(parenthes)) {
+                System.out.println("\nInvalid parentheses, try again");
+                continue;
+            }
 
 
-			System.out.println("Ans: "+topost.InfixToPostfix(input));
-		}
-	}
+            System.out.println("Ans: "+topost.InfixToPostfix(input));
+        }
+    }
 /************************************************************************/
 }
--- folder/java-backup/Triangle.java
+++ folder/java-backup/Triangle.java
@@ -5,101 +5,101 @@ package test;
 import java.util.Scanner;
 
 public class Triangle {
-	int N = 5;
-	int[][] A = new int[N][N];
-	int[] B = new int[N*(N+1)/2];
-	static Scanner keyboard = new Scanner(System.in);
-	
+    int N = 5;
+    int[][] A = new int[N][N];
+    int[] B = new int[N*(N+1)/2];
+    static Scanner keyboard = new Scanner(System.in);
+    
 /************************************************************************/
-	public void CreateDownTriangle() {
-		int tri = 0, count = 1;
+    public void CreateDownTriangle() {
+        int tri = 0, count = 1;
 
-		for(int j = 0; j < N; j++) {
-			for(int i = 0; i < N; i++) {
-				if(i >= tri) 
-					A[i][j] = count++;
-				else
-					A[i][j] = 0;
-			}
-			tri++;
-		}
-	}
+        for(int j = 0; j < N; j++) {
+            for(int i = 0; i < N; i++) {
+                if(i >= tri) 
+                    A[i][j] = count++;
+                else
+                    A[i][j] = 0;
+            }
+            tri++;
+        }
+    }
 /************************************************************************/
-	public void extract_2Dto1D(int [][] ary2D, int[] ary1D) {
-		int k = 0, n = N;
+    public void extract_2Dto1D(int [][] ary2D, int[] ary1D) {
+        int k = 0, n = N;
 
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
-				k = (i + 1) * (i - 1 + 1) / 2 + j + 1;
-				ary1D[k-1] = ary2D [i][j];
-				//System.out.println(k);
-			}
-		}
-	}
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                k = (i + 1) * (i - 1 + 1) / 2 + j + 1;
+                ary1D[k-1] = ary2D [i][j];
+                //System.out.println(k);
+            }
+        }
+    }
 /************************************************************************/
-	public int get1DVal(int[] ary1D, int i, int j) {
+    public int get1DVal(int[] ary1D, int i, int j) {
 
-		if(i<j) return 0;
-		
-		int k = (i + 1) * (i - 1 + 1) / 2 + j + 1;
-		return ary1D[k-1];
-	}
+        if(i<j) return 0;
+        
+        int k = (i + 1) * (i - 1 + 1) / 2 + j + 1;
+        return ary1D[k-1];
+    }
 /************************************************************************/
-	public void output2D(int[][] array) {
+    public void output2D(int[][] array) {
 
-		for(int i = 0; i < array.length; i++) {
-		   for(int j = 0; j < array[i].length; j++) {
-		      System.out.printf(""+array[i][j]+'\t');
-		   }
-		   System.out.println();
-		}
-	}
+        for(int i = 0; i < array.length; i++) {
+           for(int j = 0; j < array[i].length; j++) {
+              System.out.printf(""+array[i][j]+'\t');
+           }
+           System.out.println();
+        }
+    }
 /************************************************************************/
-	public void output1D(int[] array) {
-		for(int i = 0; i < array.length; i++) {
-			System.out.printf(array[i]+" ");
-		}
-		System.out.println();
-	}
+    public void output1D(int[] array) {
+        for(int i = 0; i < array.length; i++) {
+            System.out.printf(array[i]+" ");
+        }
+        System.out.println();
+    }
 /************************************************************************/
 
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Triangle triangle = new Triangle();
+        Triangle triangle = new Triangle();
 
-		
-		triangle.CreateDownTriangle();
-		
-		System.out.println("array A:");
-		triangle.output2D(triangle.A);
-		System.out.println("array B:");
-		triangle.output1D(triangle.B);
-		System.out.println("--------------------------");
+        
+        triangle.CreateDownTriangle();
+        
+        System.out.println("array A:");
+        triangle.output2D(triangle.A);
+        System.out.println("array B:");
+        triangle.output1D(triangle.B);
+        System.out.println("--------------------------");
 
-		System.out.println("extract A to B: ");
-		triangle.extract_2Dto1D(triangle.A, triangle.B);
-		System.out.println("array A:");
-		triangle.output2D(triangle.A);
-		System.out.println("array B:");
-		triangle.output1D(triangle.B);
-		System.out.println("--------------------------");
+        System.out.println("extract A to B: ");
+        triangle.extract_2Dto1D(triangle.A, triangle.B);
+        System.out.println("array A:");
+        triangle.output2D(triangle.A);
+        System.out.println("array B:");
+        triangle.output1D(triangle.B);
+        System.out.println("--------------------------");
 
-		System.out.printf("input A[i][j]:");
-		String input = keyboard.nextLine();
-		String[] input_ary = input.split(",");
-		int i = Integer.parseInt(input_ary[0]);
-		int j = Integer.parseInt(input_ary[1]);
-		
-		int result = triangle.get1DVal(triangle.B, i, j);
-		System.out.println("A["+i+"]["+j+"]= "+result);
-		
-		/*
-		System.out.println("array A:");
-		triangle.output2D(triangle.A);
-		System.out.println("array C:");
-		triangle.output2D(C);
-		*/
-	}
+        System.out.printf("input A[i][j]:");
+        String input = keyboard.nextLine();
+        String[] input_ary = input.split(",");
+        int i = Integer.parseInt(input_ary[0]);
+        int j = Integer.parseInt(input_ary[1]);
+        
+        int result = triangle.get1DVal(triangle.B, i, j);
+        System.out.println("A["+i+"]["+j+"]= "+result);
+        
+        /*
+        System.out.println("array A:");
+        triangle.output2D(triangle.A);
+        System.out.println("array C:");
+        triangle.output2D(C);
+        */
+    }
 /************************************************************************/
 }
