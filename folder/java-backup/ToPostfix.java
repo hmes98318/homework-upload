bfbc8f31bb8369e5d502d5372d096809296d74a3
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2022-11-03 21:47:16 +0800

    Update ToPostfix.java

--- folder/java-backup/ToPostfix.java
+++ folder/java-backup/ToPostfix.java
@@ -4,153 +4,189 @@ package test;
 
 import java.io.*;
 import java.util.*;
-import java.util.Stack;
 
 
 public class ToPostfix {
-    static Scanner keyboard = new Scanner(System.in);
+	static Scanner keyboard = new Scanner(System.in);
 
-    Map<Character, Integer> weight = new HashMap<Character, Integer>() {
-        {
-            put('-', 1);
-            put('+', 1);
-            put('*', 2);
-            put('/', 2);
-            put('%', 2);
-            put('^', 3);
-        }
-    };
+	Map<String, Integer> infix_weight = new HashMap<String, Integer>() {
+		{
+	        put("END", 0);
+	        put(")", 1);
+			put("-", 2);
+	        put("+", 2);
+	        put("*", 3);
+	        put("/", 3);
+	        put("%", 3);
+	        put("^", 4);
+	        put("(", 5);
+		}
+	};
 
+	Map<String, Integer> opers_weight = new HashMap<String, Integer>() {
+		{
+	        put("END", 0);
+	        put("(", 1);
+			put("-", 2);
+	        put("+", 2);
+	        put("*", 3);
+	        put("/", 3);
+	        put("%", 3);
+	        put("^", 4);
+	        //put(")", 5);
+		}
+	};
 /************************************************************************/
-    public int InfixToPostfix(String infix) {
+	public ArrayList<String> infixSplit(String infix) {
+		
+		infix = infix.replaceAll(" ", "");
+		char[] infix_ary = infix.toCharArray();
+		int infix_length = infix.length();
+		
+		ArrayList<String> list = new ArrayList<>();
+		
+		for(int i = 0; i < infix_length; i++) {
+			char c = infix_ary[i];
 
-        infix = infix.replaceAll(" ", "");
-        char[] infix_ary = infix.toCharArray();
-        int infix_length = infix.length();
-        
-        Deque<Integer> nums = new ArrayDeque<>();
-        Deque<Character> opers = new ArrayDeque<>();
-        
-        nums.addLast(0);
-        
-        for(int i = 0; i < infix_length; i++) {
-
-            if(infix_ary[i] == '(') 
-                opers.addLast(infix_ary[i]);
-
-            else if(infix_ary[i] == ')') {
-                while(!opers.isEmpty()) {
-                    if(opers.peekLast() != '(')
-                        calculate(nums, opers);
-                    else {
-                        opers.pollLast();
-                        break;
-                    }
-                }
-            }
-
-            else {
-                if(isNumber(infix_ary[i])) {
-                    int count = 0, j = i;
-                    
-                    while(j < infix_length && isNumber(infix_ary[j])) {
-                        count = count * 10 + (int)(infix_ary[j++] - '0');
-                    }
-                    i = j -1;
-                    nums.addLast(count);
-                }
-                else {
-                    while(!opers.isEmpty() && opers.peekLast() != '(') {
-                        char opersLast = opers.peekLast();
-
-                        if(weight.get(opersLast) >= weight.get(infix_ary[i]))
-                            calculate(nums, opers);
-                        else
-                            break;
-                    }
-                    opers.addLast(infix_ary[i]);
-                }
-            }
-        }
-
-        while(!opers.isEmpty() && opers.peekLast() != '(') {
-            calculate(nums, opers);
-        }
-        return nums.peekLast();
-    }
+			if(isNumber(c)) {
+				int count = 0, j = i;
+				
+				while(j < infix_length && isNumber(infix_ary[j])) {
+					count = count * 10 + (int)(infix_ary[j++] - '0');
+				}
+				i = j -1;
+				list.add(Integer.toString(count));
+			}
+			else {
+				if(isOperands(String.valueOf(c))) list.add(String.valueOf(c));
+			}
+		}
+		return list;
+	}
 /************************************************************************/
-    public void calculate(Deque<Integer> nums, Deque<Character> opers) {
-        if(nums.isEmpty() || nums.size() < 2) return;
-        if(opers.isEmpty()) return;
-        
-        int b = nums.pollLast(),
-            a = nums.pollLast();
-        char o = opers.pollLast();
-        int ans = 0;
-        
-        if(o == '+') ans = a + b;
-        else if(o == '-') ans = a - b;
-        else if(o == '*') ans = a * b;
-        else if(o == '/') ans = a / b;
-        else if(o == '%') ans = a % b;
-        else if(o == '^') ans = (int)Math.pow(a, b);
-        
-        nums.addLast(ans);
-    }
+	public ArrayList<String> infixToPostfix(ArrayList<String> infix){
+		
+		ArrayList<String> list = new ArrayList<>();
+		Deque<String> opers = new ArrayDeque<>();
+
+		opers.addLast("END");
+		
+		for(String str : infix) {
+			switch(str) {
+
+			case ")":
+				while(!opers.peekLast().equals("(")) {
+					list.add(opers.pollLast());
+				}
+				opers.pollLast();
+				break;
+
+			case"+":
+			case"-":
+			case"*":
+			case"/":
+			case"%":
+			case"(":			
+				while(opers_weight.get(opers.peekLast()) >= infix_weight.get(str)) {
+					list.add(opers.pollLast());
+				}
+				opers.addLast(str);
+				break;
+
+			default :
+				list.add(str);
+			}
+		}
+
+		while(!opers.peekLast().equals("END")) {
+			list.add(opers.pollLast());
+		}
+		return list;
+	}
 /************************************************************************/
-    public boolean isNumber(char c) {
-        return Character.isDigit(c);
-    }
+	public int postfixCalculate(ArrayList<String> postfix) {
+		
+		Deque<Integer> nums = new ArrayDeque<>();
+		int result = 0;
+		
+		for(String str : postfix) {
+			if(!isOperands(str)) {
+				int n = Integer.parseInt(str);
+				nums.addLast(n);
+			}
+			else {
+				calculate(nums, str);
+			}
+		}
+		return nums.peekLast();
+	}
 /************************************************************************/
-    public static boolean validParenthes(ArrayList<Character> parenthes) {
+	public void calculate(Deque<Integer> nums, String opers) {
+		int b = nums.pollLast(),
+			a = nums.pollLast(),
+			ans = 0;
 
-        if(parenthes.size() %2 != 0) return false;
-        
-        Stack<Character> stack = new Stack<Character>();
+		if(opers.equals("+")) ans = a + b;
+		else if(opers.equals("-")) ans = a - b;
+		else if(opers.equals("*")) ans = a * b;
+		else if(opers.equals("/")) ans = a / b;
+		else if(opers.equals("%")) ans = a % b;
+		else if(opers.equals("^")) ans = (int)Math.pow(a, b);
 
-        for(char c : parenthes) {
-            if(c == '(') 
-                stack.push(')');
-            else
-                if(stack.empty() || c != stack.pop()) return false;
-        }
-
-        if(stack.empty()) return true;
-        return false;
-    }
+		nums.addLast(ans);
+	}
+/************************************************************************/
+	public boolean isNumber(char c) {
+		return Character.isDigit(c);
+	}
+/************************************************************************/
+	public boolean isOperands(String str) {
+	    return str.equals("+") || 
+	    	   str.equals("-") || 
+	    	   str.equals("*") || 
+	    	   str.equals("/") || 
+	    	   str.equals("%") || 
+	    	   str.equals("^") || 
+	    	   str.equals("(") || 
+	    	   str.equals(")");
+	}
 /************************************************************************/
 
+	
+	public static void main(String[] args) {
+		ToPostfix postfix = new ToPostfix();
+		
+		String infix = "10*(9+1*6)";
+		
+		while(true) {
+			System.out.print("\n--------------------------\n");
+			System.out.print("   -- valid operands --\n");
+			System.out.print("  +, -, *, /, %, ^, (, )");
+			System.out.print("\n--------------------------\n");
+			
+			System.out.printf("\nEnter infix: ");
+			infix = keyboard.nextLine();
 
-    public static void main(String[] args) {
-        ToPostfix topost = new ToPostfix();
-        
-        //String infix = "10*(9+1*6)";
-        
-        while(true) {
-            System.out.print("\n--------------------------\n");
-            System.out.print("   -- valid operands --\n");
-            System.out.print("  +, -, *, /, %, ^, (, )");
-            System.out.print("\n--------------------------\n");
+			
+			ArrayList<String> infix_ary = postfix.infixSplit(infix);
+			System.out.print("Infix: ");
+			for (String str : infix_ary) {
+		    	System.out.print(str+",");
+		    }
+			
+			System.out.println();
 
+			ArrayList<String> postfix_ary = postfix.infixToPostfix(infix_ary);
+			System.out.print("Postfix: ");
+			for (String str : postfix_ary) {
+		    	System.out.print(str+",");
+		    }
 
-            System.out.printf("\nEnter infix: ");
-            String input = keyboard.nextLine();
+			System.out.println();
 
-            char[] input_ary = input.toCharArray();
-            ArrayList<Character> parenthes = new ArrayList<Character>();
-            for(int i = 0; i < input_ary.length; i++) {
-                if(input_ary[i] == '(' || input_ary[i] == ')')
-                    parenthes.add(input_ary[i]);
-            }
-
-            if(!validParenthes(parenthes)) {
-                System.out.println("\nInvalid parentheses, try again");
-                continue;
-            }
-
-
-            System.out.println("Ans: "+topost.InfixToPostfix(input));
-        }
-    }
+			int calculation_result = postfix.postfixCalculate(postfix_ary);
+			System.out.print("Ans: "+calculation_result);
+		}
+	}
 /************************************************************************/
 }
